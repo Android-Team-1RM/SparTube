bfb252b6ba765c73418de1ef510be9e75c8e3f7f
LDH <edh7054@gmail.com>
2023-09-26 20:59:00 +0900

    임시 커밋

--- app/src/main/java/com/example/naebaecamteam1rm_spartube/homepage/HomeFragment.kt
+++ app/src/main/java/com/example/naebaecamteam1rm_spartube/homepage/HomeFragment.kt
@@ -25,7 +25,6 @@ class HomeFragment : Fragment() {
     private var Q // 유튜브 검색값
             : String? = null
     private val MAX_RESULTS = 20 // 받아올 유튜브 리스트의 최대값
-    private var channelId: String? = null
     private val y_datas: ArrayList<TubeDataModel> = ArrayList() // 출력 데이터를 담을 배열
     private val c_datas: ArrayList<TubeDataModel> = ArrayList() // 출력 데이터를 담을 배열
     private val listAdapter by lazy {
@@ -93,50 +92,51 @@ class HomeFragment : Fragment() {
     fun setMostPopuler() = with(binding) {
 
         Q = "항저우 아시안게임"
-        RetrofitInstance.api.getList(Contants.MY_KEY, "snippet", Q, "video", MAX_RESULTS)?.enqueue(object :
-            Callback<VideoDTO> {
-            override fun onResponse(call: Call<VideoDTO>, response: Response<VideoDTO>) {
-                if (response.isSuccessful) {//응답 성공시 실행
-                    Log.d("test", "Response")
-                    val data = response.body()
-                    val youtubeList = data?.items
-                    if (youtubeList == null) {// 가져온 데이터 없으면 리턴
-                        return
-                    } else {
-                        for (i in youtubeList.indices) { // 가져오고 싶은 데이터 불러오고 어뎁터에 저장하는 위치
-                            val title = youtubeList.get(i).snippet.title
-                            val thumbnail = youtubeList.get(i).snippet.thumbnails.default.url
-                            val description = youtubeList.get(i).snippet.description
+        RetrofitInstance.api.getList(Contants.MY_KEY, "snippet", Q, "video", MAX_RESULTS)
+            ?.enqueue(object :
+                Callback<VideoDTO> {
+                override fun onResponse(call: Call<VideoDTO>, response: Response<VideoDTO>) {
+                    if (response.isSuccessful) {//응답 성공시 실행
+                        Log.d("test", "Response")
+                        val data = response.body()
+                        val youtubeList = data?.items
+                        if (youtubeList == null) {// 가져온 데이터 없으면 리턴
+                            return
+                        } else {
+                            for (i in youtubeList.indices) { // 가져오고 싶은 데이터 불러오고 어뎁터에 저장하는 위치
+                                val title = youtubeList.get(i).snippet.title
+                                val thumbnail = youtubeList.get(i).snippet.thumbnails.default.url
+                                val description = youtubeList.get(i).snippet.description
 //                            val url = data.etag
-                            Log.d("title", "$title")
-                            Log.d("thumbnail", "$thumbnail")
-                            Log.d("description", "$description")
+                                Log.d("title", "$title")
+                                Log.d("thumbnail", "$thumbnail")
+                                Log.d("description", "$description")
 //                            Log.d("url","$url")
 
-                            y_datas.add(
-                                TubeDataModel(
+                                y_datas.add(
+                                    TubeDataModel(
 // y_data에
-                                    title = title,
-                                    thumbnail = thumbnail,
-                                    description = description,
+                                        title = title,
+                                        thumbnail = thumbnail,
+                                        description = description,
 
-                                    )
-                            )
-                            Log.d("y_datas", "$y_datas")
-                            listAdapter.list = y_datas //리스트를 어댑터에 적용
-                            listAdapter.notifyDataSetChanged()// notity
+                                        )
+                                )
+                                Log.d("y_datas", "$y_datas")
+                                listAdapter.list = y_datas //리스트를 어댑터에 적용
+                                listAdapter.notifyDataSetChanged()// notity
 
+                            }
                         }
+
                     }
-
                 }
-            }
 
-            override fun onFailure(call: Call<VideoDTO>, t: Throwable) {//실패시 찍히는 로그
-                Log.d("test1", "fail")
-            }
+                override fun onFailure(call: Call<VideoDTO>, t: Throwable) {//실패시 찍히는 로그
+                    Log.d("test1", "fail")
+                }
 
-        })
+            })
 
     }
 
@@ -144,7 +144,13 @@ class HomeFragment : Fragment() {
         Q = "항저우 아시안게임"
         //val keyword: String = "야구"
         //channelId = "UCnXNukjRxXGD8aeZGRV-lYg" //스포타임 채널 ID
-        RetrofitInstance.api.getchannelList(Contants.MY_KEY, "snippet", Q/*+ keyword.toString()*/, "channel", /*channelId,*/ MAX_RESULTS)
+        RetrofitInstance.api.getchannelList(
+            Contants.MY_KEY,
+            "snippet",
+            Q/*+ keyword.toString()*/,
+            "channel", /*channelId,*/
+            MAX_RESULTS
+        )
             ?.enqueue(object :
                 Callback<VideoDTO> {
                 override fun onResponse(call: Call<VideoDTO>, response: Response<VideoDTO>) {
@@ -191,7 +197,8 @@ class HomeFragment : Fragment() {
             })
 
     }
-    fun modifyItemToAddFavorite(item: TubeDataModel){
+
+    fun modifyItemToAddFavorite(item: TubeDataModel) {
         listAdapter.modifyItemToAddFavorite(item)
     }
 }
