160e4d01867547753561cfc41a010b876b8eac60
rising111 <ejttotto1234@gmail.com>
2023-09-26 11:47:33 +0900

    [FEAT] : ⚙️ 프래그먼트에 어뎁터 장착

--- app/src/main/java/com/example/naebaecamteam1rm_spartube/mypage/MyPageFragment.kt
+++ app/src/main/java/com/example/naebaecamteam1rm_spartube/mypage/MyPageFragment.kt
@@ -1,10 +1,12 @@
-package com.example.naebaecamteam1rm_spartube
+package com.example.naebaecamteam1rm_spartube.mypage
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.naebaecamteam1rm_spartube.databinding.FragmentMyPageBinding
 
 class MyPageFragment: Fragment() {
@@ -13,7 +15,13 @@ class MyPageFragment: Fragment() {
     }
     private var _binding : FragmentMyPageBinding? =null
     private val binding get() = _binding!!
-
+    private val listAdapter by lazy{
+        MyPageAdapter()
+    }
+    private val viewModel: MyPageViewModel by lazy{
+        ViewModelProvider(this)[MyPageViewModel::class.java]
+    }
+    private lateinit var gridmanager:StaggeredGridLayoutManager
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -25,10 +33,26 @@ class MyPageFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initView()
+        initViewModel()
     }
 
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
     }
+    private fun initView() = with(binding){
+        gridmanager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        recyclerview.layoutManager =gridmanager
+        recyclerview.adapter = listAdapter
+
+    }
+    private fun initViewModel(){
+        with(viewModel){
+            list.observe(viewLifecycleOwner){
+                listAdapter.submitList(it)
+            }
+        }
+    }
+
 }
