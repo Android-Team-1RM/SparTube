173ef4def81a5efae1b11be58cccf10da3adb220
LDH <edh7054@gmail.com>
2023-10-02 15:58:34 +0900

    임시 UI 커밋

--- app/src/main/java/com/example/naebaecamteam1rm_spartube/homepage/HomeFragment.kt
+++ app/src/main/java/com/example/naebaecamteam1rm_spartube/homepage/HomeFragment.kt
@@ -72,8 +72,8 @@ class HomeFragment : Fragment() {
 
     private fun initView() = with(binding) {
         setMostPopulerVideo() // 모스트 파퓰러
-        setMostPopulerShorts() // 쇼츠
-        setCategoryCannels() // 카테고리 채널
+//        setMostPopulerShorts() // 쇼츠
+//        setCategoryCannels() // 카테고리 채널
 
         vmanager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
         recyclerMpVideo.layoutManager = vmanager
