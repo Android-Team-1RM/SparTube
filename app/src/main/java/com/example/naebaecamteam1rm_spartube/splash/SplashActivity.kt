65fb13246d748e034d55e9ab090654fa23154f67
LDH <edh7054@gmail.com>
2023-10-05 09:55:32 +0900

    ui 수정

---
+++ app/src/main/java/com/example/naebaecamteam1rm_spartube/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.naebaecamteam1rm_spartube.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.naebaecamteam1rm_spartube.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
