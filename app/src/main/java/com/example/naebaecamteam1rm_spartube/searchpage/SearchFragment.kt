3d92245494739f094507d82f03129333f42ef826
seunghoon Lee <hara9052@naver.com>
2023-10-05 16:44:33 +0900

    [FEAT] : ⚙️ SearchFragment infinite

--- app/src/main/java/com/example/naebaecamteam1rm_spartube/searchpage/SearchFragment.kt
+++ app/src/main/java/com/example/naebaecamteam1rm_spartube/searchpage/SearchFragment.kt
@@ -1,6 +1,7 @@
 package com.example.naebaecamteam1rm_spartube.searchpage
 
 import android.content.Context
+import android.os.Build.VERSION_CODES.Q
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView.OnQueryTextListener
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
@@ -34,7 +36,8 @@ class SearchFragment : Fragment() {
     private lateinit var mContext: Context
     private lateinit var adapter: SearchAdapter
     private val youDatas: ArrayList<TubeDataModel> = ArrayList() // 출력 데이터를 담을 배열
-    private lateinit var gridmanager: StaggeredGridLayoutManager
+    private var nextPageToken: String? = null
+    private var currentNextPageToken: String? = null
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -56,7 +59,6 @@ class SearchFragment : Fragment() {
         setRv()
         updateBtn()
         binding.ivRefresh.setOnClickListener { updateBtn() }
-        setRecyclerViewScrollListener()
     }
 
     //키워드 버튼을 눌렀을 때 각 키워드로 검색
@@ -72,14 +74,17 @@ class SearchFragment : Fragment() {
         firstBtn.setOnClickListener {
             adapter.clearItem()
             settest(firstBtn.text.toString())
+            infiniteScroll(firstBtn.text.toString())
         }
         secondBtn.setOnClickListener {
             adapter.clearItem()
             settest(secondBtn.text.toString())
+            infiniteScroll(secondBtn.text.toString())
         }
         thirdBtn.setOnClickListener {
             adapter.clearItem()
             settest(thirdBtn.text.toString())
+            infiniteScroll(thirdBtn.text.toString())
         }
     }
 
@@ -109,16 +114,16 @@ class SearchFragment : Fragment() {
 
     //리사이클러뷰
     fun setRv() {
-        gridmanager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL)
         adapter = SearchAdapter(mContext)
         binding.rvSearchResult.adapter = adapter
-        binding.rvSearchResult.layoutManager = gridmanager
+        binding.rvSearchResult.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
 
         adapter.itemClick = object : SearchAdapter.ItemClick {
             override fun onClick(view: View, tubeData: TubeDataModel) {
                 startActivity(VideoDetailPageActivity.VideoDetailPageNewIntent(context, tubeData))
             }
         }
+        infiniteScroll(binding.rvSearchResult.toString())
 
     }
 
@@ -140,6 +145,7 @@ class SearchFragment : Fragment() {
                         if (youtubeList == null) {
                             return
                         } else {
+                            nextPageToken = data?.nextPageToken
                             for (i in youtubeList.indices) {
                                 var title = youtubeList.get(i).snippet.title
                                 title = title.replace("&#39;", "'")
@@ -178,18 +184,87 @@ class SearchFragment : Fragment() {
             })
         }
 
+
     }
 
-    fun setRecyclerViewScrollListener() {
+    fun setNextTest(search: String) = with(binding) {
+        CoroutineScope(Dispatchers.IO).launch {
+                RetrofitInstance.api.getNextList(
+                    Contants.MY_KEY,
+                    "snippet",
+                    search,
+                    "video",
+                    nextPageToken,
+                    MAX_RESULTS
+                )?.enqueue(object :
+                    Callback<VideoDTO> {
+                    override fun onResponse(call: Call<VideoDTO>, response: Response<VideoDTO>) {
+                        if (response.isSuccessful) {
+                            Log.d("test", "Response")
+                            val data = response.body()
+                            val youtubeList = data?.items
+                            if (youtubeList == null) {
+                                return
+                            } else {
+                                nextPageToken = data?.nextPageToken
+                                for (i in youtubeList.indices) {
+                                    var title = youtubeList.get(i).snippet.title
+                                    title = title.replace("&#39;", "'")
+                                    title = title.replace("&quot;", "\"")
+                                    val channelId = youtubeList.get(i).snippet.channelId
+                                    val description = youtubeList.get(i).snippet.description
+                                    val thumbnail = youtubeList.get(i).snippet.thumbnails.high.url
+                                    val channeltitle = youtubeList.get(i).snippet.channelTitle
+                                    val videoID = youtubeList.get(i).id.videoId
+                                    var url = "https://www.youtube.com/watch?v=" + videoID
+                                    Log.d("title", "$title")
+                                    Log.d("url", "$thumbnail")
+                                    youDatas.add(
+                                        TubeDataModel(
+                                            title = title,
+                                            thumbnail = thumbnail,
+                                            description = description,
+                                            channelName = channeltitle,
+                                            url = url,
+                                            channelId = channelId
+                                        )
+                                    )
+                                    Log.d("y_datas", "$youDatas")
+                                    adapter.items = youDatas
+                                    adapter.notifyDataSetChanged()
+                                }
+                            }
+                        }
+
+                    }
+
+                    override fun onFailure(call: Call<VideoDTO>, t: Throwable) {
+                        Log.d("test1", "fail")
+                    }
+
+                })
+            }
+
+
+    }
+
+
+    private fun infiniteScroll(search: String) {
         binding.rvSearchResult.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
                 super.onScrollStateChanged(recyclerView, newState)
-                if (recyclerView.canScrollVertically(-1)) {
-                    Log.d("infinite scroll", "hi")
+                val lastVisibleItemPosition =
+                    (binding.rvSearchResult.layoutManager as LinearLayoutManager?)!!.findLastCompletelyVisibleItemPosition()
+                val itemCount = adapter!!.itemCount - 1
+                if (!binding.rvSearchResult.canScrollVertically(1) && lastVisibleItemPosition == itemCount) {
+                    Log.d("endScroll", "endScroll")
+                    setNextTest(search)
+
                 }
             }
         })
     }
+
     fun modifyItemToAddFavorite(item: TubeDataModel) {
         adapter.modifyItemToAddFavorite(item)
     }
